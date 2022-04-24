aef8516a956dfc6a5d2469bde0b1daec14b903f5
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-04-24 18:09:17 -0300

    feat: new design reponsive page DetailsCourse

--- src/pages/details/DetailsCourse.js
+++ src/pages/details/DetailsCourse.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid,CardContent,CardActions, IconButton} from "@material-ui/core";
 import classnames from "classnames";
-
+import Card from '@material-ui/core/Card';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
 // styles
 import "react-toastify/dist/ReactToastify.css";
 import useStyles from "./styles";
 
 // components
-import Widget from "../../components/Widget/Widget";
-import PageTitle from "../../components/PageTitle/PageTitle";
-import Notification from "../../components/Notification";
+
 import { Typography, Button } from "../../components/Wrappers/Wrappers";
 import CardMedia from '@material-ui/core/CardMedia';
 import api from '../../services/api'
 
-export default function NotificationsPage(props) {
+export default function NotificationsPage (props) {
   var classes = useStyles();
   const [course, setCourse] = useState([])
   const [courseClasses, setCourseClasses] = useState([])
 
   useEffect(() => {
 
-    async function fetchData() {
+    async function fetchData () {
       await api.get(`/v1/courses/${1}`, {
       })
         .then((response) => {
@@ -37,51 +37,74 @@ export default function NotificationsPage(props) {
 
   return (
     <>
-      <Grid container spacing={4}>
-        <Grid item xs={8} >
-            <Widget disableWidgetMenu>
-              <Grid container item xs={12} spacing={4}>
-                   <Grid item xs={5}>
-                   <CardMedia
-                  component="img"
-                  height="200"
-                  image="https://escuelafullstack.com/web/image/slide.channel/18/image_512"
-                  alt="green iguana"
-                />
-                    </Grid>
-                    <Grid item xs={7}>
-                    <Widget title={course.name} noWidgetShadow disableWidgetMenu noBodyPadding noHeaderPadding style={{paddingRight: 15}} headerClass={classes.widgetHeader}>
-                      <Typography>
-                        {course.description}
-                      </Typography>
-                      <Typography>
-                        {courseClasses.length} aula(s).
-                      </Typography>
-                   
-                      
-                    </Widget>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Widget title="Conteudo"disableWidgetMenu noWidgetShadow>
-                      {courseClasses.map(course => (
-                              <Grid key={course.id} item xs={4} >
-                              <Widget title={course.name}  disableWidgetMenu noBodyPadding noHeaderPadding style={{paddingRight: 15}} headerClass={classes.widgetHeader}>
-                                <Typography>
-                                  {course.description}
-                                </Typography>
-                              </Widget>
-                              </Grid>
-                      ))}
-                        </Widget>
-                      </Grid>
-                   
-              </Grid>
-            </Widget>
+  <Grid container spacing={1} >
+      <Grid item sm={4} md={4}>
+          <Card style={{
+          boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+          backgroundColor: "#fafafa",
+        }}>
+          <CardMedia
+            component="img"
+            maxWidth= "345"
+            maxHeight= "345"
+            image="https://escuelafullstack.com/web/image/slide.channel/18/image_512"
+            alt="green iguana"
+          />
+        </Card>
         </Grid>
-        <Grid item xs={4}>
-        <Widget disableWidgetMenu>
-      
-        </Widget>
+
+        <Grid item sm={8} md={8}>
+        <CardContent>
+                  <Typography gutterBottom variant="h1" component="div">
+                    {course.name}
+                  </Typography>
+                  <Typography variant="body1" color="text.primary" >
+                    {courseClasses.length} aula(s)
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary" >
+                    {course.description} Um curso totalmente focado em Java para Web!
+
+Com ele você aprenderá a desenvolver seus próprios websites e sistemas com essa linguagem!
+
+Tudo o que há de mais recente nessa tecnologia você encontra em nosso curso que é totalmente focado em projetos práticos para você desenvolver aplicações poderosas com o Java no Back-end.
+                  </Typography>
+                
+                <CardActions disableSpacing>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className={classes.buttonsContainer}
+                >
+                Inscrever-se
+                </Button>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+                </CardContent>
+        </Grid>
+        <Grid item>
+          <CardContent>
+                  <Typography gutterBottom variant="h2" component="div">
+                  Conteudo
+                  </Typography>
+                  {courseClasses.map(course => (
+              <Card key={course.id}>
+                 <CardContent>
+                 <Typography  variant="h4" component="p">
+                 {course.id} - {course.name}
+                  </Typography>
+                <Typography>
+                  {course.description} Neste curso o aluno irá aprender as partes básicas como: Configuração do Ambiente; Algoritmo e Estrutura de Dados; Fundamentos da Linguagem Java; Estruturas de Controle; Classes, Objetos e Métodos. Dessa forma, o aluno estará preparado para conceitos mais avançados, como a Orientação a Objetos, por exemplo.
+                </Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </CardContent>
         </Grid>
       </Grid>
     </>
