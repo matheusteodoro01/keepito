031a6020affb36021670f213a4611a6f94d039e2
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-06-17 20:20:11 -0300

    chore: update backend link

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const token = localStorage.getItem("keepitoAuthorization");
 const api = axios.create({
-  baseURL: "http://45.79.204.34:8080",
+  baseURL: "http://https://jornada-back.herokuapp.com",
 });
 
 if (token) {
   api.defaults.headers.authorization = `${token}`
 }
-
+api.version = "/v1/";
 export default api;
